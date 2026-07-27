8db61ed9c7fa16e8feb2e320fcf2e8b23435025d
aliakbarr474 <aliakbarr474@gmail.com>
2026-07-28 01:03:32 +0500

    Updated mission section responsiveness and added SEO meta tags

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -9,7 +9,6 @@ const routes = [
   '/software'
 ];
 
-// Map the routes into XML formatting
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${routes.map(route => `
@@ -19,6 +18,5 @@ const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 </urlset>
 `;
 
-// Write the file into the CRA build folder
 fs.writeFileSync('build/sitemap.xml', sitemap);
 console.log('Sitemap generated successfully!');
--- src/Mission.js
+++ src/Mission.js
@@ -6,7 +6,7 @@ export default function Mission(){
             <div className='mission-div'>
                 <div className='mission-heading'>
                     <h3>Sarte's Mission</h3>
-                    <h1>Driving excellence through <br></br>vision and precision.</h1>
+                    <h1>Driving excellence through <br />vision and precision.</h1>
                     
                     <p>Excellence isn’t just our goal — it’s our way of working. With a clear vision that inspires progress and a commitment to precision that perfects every detail, we deliver solutions that truly stand apart. At Sarte Groups, we turn ambition into achievement.</p>
                 </div>
@@ -20,7 +20,7 @@ export default function Mission(){
 
                 <div className='mission-heading'>
                     <h3>The Sarte Edge</h3>
-                    <h1>Innovation That Commands<br></br> Respect</h1>
+                    <h1>Innovation That Commands<br /> Respect</h1>
                     
                     <p>We believe innovation should do more than impress — it should earn trust. At Sarte Groups, our ideas are crafted with purpose, built on expertise, and refined with integrity, creating solutions that inspire confidence and command respect.</p>
                 </div>
