bea21b63e5264234f15d85ce8403021aeccdf31c
aliakbarr474 <aliakbarr474@gmail.com>
2026-09-21 09:55:17 +0500

    Constructions page hero responsivised

--- src/CHero.js
+++ src/CHero.js
@@ -1,12 +1,14 @@
 import './constructionHero.css'
 import logo from './assets/logo.png'
 
-export default function CHero(){
+export default function CHero() {
     return (
-        <div>
-            <div className="construction-hero-main">
-                <img src={logo}></img>
-            </div>
-        </div>
+        <section className="construction-hero-main">
+            <img
+                className="construction-hero-logo"
+                src={logo}
+                alt="Company Logo"
+            />
+        </section>
     )
 }
