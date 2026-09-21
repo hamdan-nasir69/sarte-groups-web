36b7d4b92906a9fbb55fdf5a265307e25ae8c906
aliakbarr474 <aliakbarr474@gmail.com>
2026-09-21 09:47:28 +0500

    Right path usage

--- src/Marketing/Hero.js
+++ src/Marketing/Hero.js
@@ -1,10 +1,10 @@
 import './hero.css'
-import logo from '../assets/Sarte Constructions Logo.png'
+import logo from '../assets/Sarte Marketing Logo No Background.png'
 
 export default function Hero(){
     return (
         <div className="marketing-hero">
-            <img src={logo}></img>
+            <img src={logo} alt='Sarte Marketing Logo'></img>
         </div>
     )
 }
